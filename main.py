704918c96addddd32375969fa0671ab4cb17f1b2
Nikolas <naurgul@gmail.com>
2013-04-02 21:32:04 +0200

    Forced some positions to never be considered via dummy characters.
    - There are two possible dummy characters for each word. One at the beginning and one at the end. We calculate when they are needed and place them appropriately.
    - Made the final crossword output a bit more readable.
    - Made the code slightly more readable by adding some comments and changing the layout a bit.

--- main.py
+++ main.py
@@ -17,13 +17,12 @@ def importDictionary():
 def generateCrossword(dimensions, lexicon):
     maxHeight, maxWidth = dimensions
     grid = [[""]*maxWidth for _ in range(maxHeight)]
-    print grid
     terms = []
     for _ in range(10):
         #set the position and direction for the new word
         startRow = random.randint(0, maxHeight - 1)
         startCol = random.randint(0, maxWidth  - 1)
-        across = random.random() > 0.5
+        across = random.random() > 0.5 # select direction for the word: either across or down
         
         #find all crossing points with pre-existing words
         letters = []
@@ -33,31 +32,39 @@ def generateCrossword(dimensions, lexicon):
             letters = [row[startCol] for row in grid[startRow:]]
         conditions = [(index, letter) for index, letter in enumerate(letters) if len(letter) > 0]
         #print "Conditions: " + str(conditions) + ", starting row: " + str(startRow) + ", starting column: " + str(startCol)
-        
-        #find all the words that satisfy the above conditions
+        # decide whether a dummy character is needed in the beginning of the word
+        startdummy = True
+        if across and startCol == 0 and not grid[startRow][startCol] == dummy:
+            startdummy = False
+        elif not across and startRow == 0 and not grid[startRow][startCol] == dummy:
+            startdummy = False  
+        # calculate the maximum length of the word
         maxWordLength = maxWidth - startCol if across else maxHeight - startRow
         if maxWordLength < 2: continue #again, can't have one-letter words
+        
+        #find a word that satisfies the above conditions, starting from a random place in the lexicon
         firstEntry = random.randint(0, len(lexicon) - 1)
         for term, definition in lexicon[firstEntry:]:
-            if len(term) > maxWordLength or term in terms: continue
-            term = term + dummy # extend the term with a dummy character
+            if startdummy: term = dummy + term 
+            if len(term) > maxWordLength or term in terms: 
+                continue
+            elif len(term) < maxWordLength:
+                term = term + dummy
             satisfiedConditions = sum(i >= len(term) or term[i] == letter for i, letter in conditions)
             if satisfiedConditions == len(conditions):
-                terms.append(term[:-1])
+                terms.append(term.replace(dummy, ''))
                 for offset, letter in enumerate(term):
-                    if across and startCol + offset < maxWidth:
+                    if across:
                         grid[startRow][startCol + offset] = letter
-                    elif startRow + offset < maxHeight:
+                    else:
                         grid[startRow + offset][startCol] = letter
                 #print term
                 #print "Crossword:\n" + "\n".join(str(row) for row in grid) #newline for each row
-                break #word found
+                break #word found 
     return grid, terms
 
-def test():
-    pass
 
 lexicon = importDictionary()
 grid, terms = generateCrossword((5,5), lexicon)
-print "CROSSWORD:\n" + "\n".join(str(row) for row in grid)
-print "TERMS:" + str(terms)
+print "CROSSWORD:\n\n" + "\n".join(' '.join([x if len(x)>0 else '_' for x in row]) for row in grid)
+print "\nTERMS: " + ', '.join(terms)
