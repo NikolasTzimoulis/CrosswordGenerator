7ff3817e757139a4adef2ee622b2a6dda9fc5169
Nikolas <naurgul@gmail.com>
2013-04-03 03:26:40 +0200

    Backtracking!
    - Added states. Each state includes the grid and the terms. If a word is added to the crossword then it is recorded as a new state and added to the front. If a certain state leads to no words found that can be added, then the state is removed from the front and added to the dead-end list.
    - Lexicon is now indexed by word length for faster search. When trying to add a new word, we start our search from the sub-lexicon including words of maximum length and decrease the word length one by one every time the search is fruitless.
    - Increased max rounds to 10,000.
    - minimum word length is now a constant.

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 # This Python file uses the following encoding: utf-8
 
-import random, re
+import random, copy
 
 dummy = '@'
-maxRounds = 100
+maxRounds = 10000
+minWordLength = 2
 
 def importDictionary():
     filename = 'dictionary-en.tsv'
-    dictionary = open(filename, 'r')
-    lexicon = []
-    for line in dictionary:
+    dictionaryRaw = open(filename, 'r')
+    lexicon = {}
+    for line in dictionaryRaw:
         lang, term, pos, definition = line.split('\t')
-        if len(term) >= 2 and term.isalpha():
-            lexicon.append((term.upper(), definition))        
+        wordLength = len(term)
+        if wordLength >= minWordLength and term.isalpha():
+            if wordLength not in lexicon: lexicon[wordLength] = [] 
+            lexicon[wordLength].append((term.upper(), definition))
     return lexicon
 
 def generateCrossword(dimensions, lexicon):
     maxHeight, maxWidth = dimensions
-    grid = [[""]*maxWidth for _ in range(maxHeight)]
     terms = {}
+    grid = [[""]*maxWidth for _ in range(maxHeight)]
+    frontStates = [ (terms, grid) ]
+    deadEndStates = []
     for i in range(maxRounds):
+        terms, grid = copy.deepcopy(frontStates[-1])
         if i%(maxRounds/10)==0: print '.',
         #set the position and direction for the new word
         across = random.random() > 0.5 # select direction for the word: either across or down
         # find position closest to left-uppermost corner that does not have a word starting from it 
         startRow = startCol = 0
-        while (startRow, startCol, across) in terms.keys():
+        while (startRow, startCol, across) in terms:
             if startRow == startCol:
                 startRow += 1
             elif startRow > startCol:
@@ -49,27 +55,40 @@ def generateCrossword(dimensions, lexicon):
             startdummy = False  
         # calculate the maximum length of the word
         maxWordLength = maxWidth - startCol if across else maxHeight - startRow
-        if maxWordLength < 2: continue #again, can't have one-letter words
+        if maxWordLength < minWordLength: continue #again, can't have one-letter words
         
         #find a word that satisfies the above conditions, starting from a random place in the lexicon
-        firstEntry = random.randint(0, len(lexicon) - 1)
-        for term, definition in lexicon[firstEntry:]:
-            if startdummy: term = dummy + term 
-            if len(term) > maxWordLength or term in terms: 
-                continue
-            elif len(term) < maxWordLength:
-                term = term + dummy
-            satisfiedConditions = sum(i >= len(term) or term[i] == letter for i, letter in conditions)
-            if satisfiedConditions == len(conditions):
-                terms[(startRow, startCol, across)] = term.replace(dummy, '')
-                for offset, letter in enumerate(term):
-                    if across:
-                        grid[startRow][startCol + offset] = letter
-                    else:
-                        grid[startRow + offset][startCol] = letter
-                #print term
-                #print "Crossword:\n" + "\n".join(str(row) for row in grid) #newline for each row
-                break #word found 
+        wordFound = False
+        for wordLength in reversed(range(minWordLength, maxWordLength+1)):
+            if wordFound: break
+            subLexicon = lexicon[wordLength]
+            startEntry = random.randint(0, len(subLexicon) - 1)
+            # the direction of search is also random
+            lexIter = subLexicon[startEntry:] if random.random() > 0.5 else reversed(subLexicon[:startEntry])
+            for term, definition in lexIter:
+                if startdummy: term = dummy + term 
+                if len(term) > maxWordLength or term in terms.values(): 
+                    continue
+                elif len(term) < maxWordLength:
+                    term = term + dummy
+                satisfiedConditions = sum(i >= len(term) or term[i] == letter for i, letter in conditions)
+                if satisfiedConditions == len(conditions):
+                    terms[(startRow, startCol, across)] = term.replace(dummy, '')
+                    for offset, letter in enumerate(term):
+                        if across:
+                            grid[startRow][startCol + offset] = letter
+                        else:
+                            grid[startRow + offset][startCol] = letter
+                    #print term
+                    #print "Crossword:\n" + "\n".join(str(row) for row in grid) #newline for each row
+                    newState = (terms, grid)
+                    if newState not in deadEndStates:
+                        frontStates.append(newState)
+                        #printCrossWord(grid)
+                        wordFound = True
+                    break #word found
+        if not wordFound: # we need to backtrack!
+            deadEndStates.append( frontStates.pop() )
     return grid, terms
 
 def printCrossWord(grid):
