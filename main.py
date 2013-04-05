74e174577b314efbb2c8a8a22d0f55d6f3974dd0
Nikolas <naurgul@gmail.com>
2013-04-05 02:35:50 +0200

    Extra layer of indexing for faster word search
    - Lexicon now stores words like this: First a layer with word lengths. For each length you have a hash table with (position,letter) pairs storing a list of all the words that have that letter in that position.
    - Each value in the lexicon is now a id number of a word. The number can be exchanged for the corresponding word and definition in a separate look-up table.
    - Searching the lexicon now works like this: First a list of valid words is retrieved for each condition (position, letter). Then, a word is chosen randomly from the intersection of these lists.
    - Because the way words are compared with conditions is fundamentally different, dummy chars work differently now. First, the minimum and maximum word length is calculated for a given position, taking dummy characters into account. Then, only words in that range are retrieved from the lexicon. The algorithm never explicitly checks dummy characters from pre-existing words (conditions).
    - Changed default number of rounds to 100,000.
    - Added comments to isValidStart.
    - Added compiled python files to the ignore-list.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import decorators
 from decorators import profile
 
 dummy = '@'
-maxRounds = 100
-minWordLength = 2
+maxRounds = 10**5
+minWordLength_absolute = 2
 maxCandidates = 3
 profiled = decorators.profiled
 
@@ -15,16 +15,22 @@ def importDictionary():
     filename = 'dictionary-en.tsv'
     dictionaryRaw = open(filename, 'r')
     lexicon = {}
+    wordLookup = {}
+    wordId = 0
     for line in dictionaryRaw:
         lang, term, pos, definition = line.split('\t')
         wordLength = len(term)
-        if wordLength >= minWordLength and term.isalpha():
-            if wordLength not in lexicon: lexicon[wordLength] = [] 
-            lexicon[wordLength].append((term.upper(), definition))
-    return lexicon
+        if wordLength >= minWordLength_absolute and term.isalpha():
+            if wordLength not in lexicon: lexicon[wordLength] = {} 
+            for index, letter in enumerate(term):
+                if (index, letter.upper()) not in lexicon[wordLength]: lexicon[wordLength][(index, letter.upper())]=set([])
+                lexicon[wordLength][(index, letter.upper())].add(wordId)
+                wordLookup[wordId] = (term.upper(), definition)
+            wordId += 1
+    return lexicon, wordLookup
 
 @profile
-def generateCrossword(size, lexicon):
+def generateCrossword(size, lexicon, wordLookup):
     maxHeight = maxWidth = size
     startOrder = set(itertools.combinations_with_replacement(range(size),2))
     startOrder = startOrder.union([(x[1], x[0]) for x in startOrder])
@@ -61,45 +67,53 @@ def generateCrossword(size, lexicon):
             startdummy = False
         elif not across and startRow == 0 and not grid[startRow][startCol] == dummy:
             startdummy = False  
-        # calculate the maximum length of the word
-        maxWordLength = maxWidth - startCol if across else maxHeight - startRow
-        if maxWordLength < minWordLength: continue #again, can't have one-letter words
+        # calculate the minimum and maximum length of the word 
+        for maxWordLength in range(minWordLength_absolute, maxWidth - startCol + 1 if across else maxHeight - startRow + 1):
+            if (maxWordLength-1, dummy) in conditions:
+                maxWordLength -= 1 # decrease by one because the iterator will increment it again on its own before breaking
+                break
+        for minWordLength in range(minWordLength_absolute, maxWordLength+1):
+            if minWordLength-1 not in [x[0] for x in conditions] or (minWordLength-1, dummy) in conditions: 
+                minWordLength -= 1 # decrease by one because the iterator will increment it again on its own before breaking
+                break    
+        if maxWordLength < minWordLength_absolute: continue #again, can't have one-letter words
         
         #find a word that satisfies the above conditions
         wordFound = False
         # iterate through all valid word lengths
         for wordLength in reversed(range(minWordLength, maxWordLength+1)):
-            if wordFound: break
+            # words of maxWordLength should only be considered if we don't need a dummy char at the beginning of the word
+            if startdummy and wordLength == maxWordLength: continue 
             subLexicon = lexicon[wordLength]
-            if len(conditions) < 2:
-                # start from a random place in the sub-lexicon
-                startEntry = random.randint(0, len(subLexicon) - 1)
-                # the direction of search is also random
-                lexIter = subLexicon[startEntry:] if random.random() > 0.5 else reversed(subLexicon[:startEntry])
-            else:
-                lexIter = subLexicon if random.random() > 0.5 else reversed(subLexicon)
-            for term, definition in lexIter:
+            # only conditions up to the wordLength-th word need apply
+            subConditions = filter(lambda x: x[0]<wordLength, conditions) 
+            # calculate the lists of words that satisfy each condition
+            # their intersection are the words that satisfy all conditions 
+            if len(subConditions) > 0: 
+                fittingWordSets = map(lambda x: subLexicon[ (x[0]+startdummy, x[1]) ], subConditions)
+                fittingWordIds = reduce(lambda x,y: x.intersection(y), fittingWordSets)
+            else: # if there are no conditions, take all words!
+                fittingWordSets = subLexicon.values()
+                fittingWordIds = reduce(lambda x,y: x.union(y), fittingWordSets)
+            fittingWords = map(lambda x: wordLookup[x][0], fittingWordIds)
+            if len(fittingWords) > 0:
+                term = random.choice(fittingWords) # choose one of the fitting words randomly
+                terms[(startRow, startCol, across)] = term # add new word to list of terms
+                # add dummy chars at the beginning and end of the new word as necessary
                 if startdummy: term = dummy + term 
-                if len(term) > maxWordLength or term in terms.values(): 
-                    continue
-                elif len(term) < maxWordLength:
-                    term = term + dummy
-                satisfiedConditions = sum(i >= len(term) or term[i] == letter for i, letter in conditions)
-                if satisfiedConditions == len(conditions):
-                    terms[(startRow, startCol, across)] = term.replace(dummy, '')
-                    for offset, letter in enumerate(term):
-                        if across:
-                            grid[startRow][startCol + offset] = letter
-                        else:
-                            grid[startRow + offset][startCol] = letter
-                    #print term
-                    #print "Crossword:\n" + "\n".join(str(row) for row in grid) #newline for each row
-                    newState = (terms, grid)
-                    if newState not in deadEndStates:
-                        frontStates.append(newState)
-                        #printCrossWord(grid)
-                        wordFound = True
-                    break #word found
+                if len(term) < maxWordLength: term = term + dummy
+                # place new term on grid
+                for offset, letter in enumerate(term):
+                    if across:
+                        grid[startRow][startCol + offset] = letter
+                    else:
+                        grid[startRow + offset][startCol] = letter
+                newState = (copy.deepcopy(terms), copy.deepcopy(grid))
+                if newState not in deadEndStates:
+                    frontStates.append(newState)
+                    #printCrossWord(grid)
+                    wordFound = True
+                    break
         if not wordFound: # we need to backtrack!
             deadEndStates.append( frontStates.pop() )
     for t, g in frontStates + deadEndStates: #find best state!
@@ -109,9 +123,13 @@ def generateCrossword(size, lexicon):
 
 @profile
 def isValidStart(grid, terms, startRow, startCol, across):
+    # iterate backwards beginning from the starting position
     it = reversed(range( (startCol if across else startRow) + 1 )) 
     for i in it:
         r, c = (startRow, i) if across else (i, startCol)
+        # if you find a starting position of a previous word 
+        # before you find a dummy char or the edge of the crossword
+        #then this starting position is invalid 
         if (r, c, across) in terms:
             return False
         elif grid[r][c] == dummy:
@@ -126,13 +144,13 @@ def getConditions(grid, startRow, startCol, across):
     else:
         letters = [row[startCol] for row in grid[startRow:]]
     return [(index, letter) for index, letter in enumerate(letters) if len(letter) > 0]
+    
 
 def printCrossWord(grid):
     print "\nCrossword:\n\n" + "\n".join(' '.join([x if len(x)>0 else '_' for x in row]) for row in grid)
 
-lexicon = importDictionary()
-grid, terms = generateCrossword(5, lexicon)
+lexicon, wordLookup = importDictionary()
+grid, terms = generateCrossword(5, lexicon, wordLookup)
 printCrossWord(grid)
 print "\nTerms: " + ', '.join(terms.values())
-
 decorators.printProfiled()
