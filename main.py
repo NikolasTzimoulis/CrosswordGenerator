c6d2a564350e04d8deb96666b3f7262a593ff6bd
Nikolas <naurgul@gmail.com>
2013-04-14 03:46:52 +0200

    Bug fixes and improvements: fixed #1, fixed #2, fixed #7
    - Crossing conditions now immediately adjust to take the existence of a start dummy into account. Behaviour across the board is less finicky as a consequence.
    - Instead of choosing one word for a particular length and then moving to the next word length if that word is found to be lacking, now we check all fitting words of a particular length in random order before we start trying words of smaller lengths.
    - Choosing the next starting position is no longer deterministic. Instead, a starting position is chosen with probability proportional to how many potential crossings the word to be placed can have.
    - Additional check in isValidStart() to prevent bug that allowed invalid starting positions.
    - getAllowedWordLengths now takes starting dummy character into account if active.
    - Removing dummy chars from conditions should not pose any problems now.
    - Refactored checking for too small word lengths. Now it should be less problematic.
    - Getting the best state at the end has its own separate function now: getBestState().
    - Increased number of candidate starting positions for each round.
    - Removed checks for ovewrites now that #1 seems to be fixed.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from decorators import profile
 dummy = '@'
 maxRounds = 10**3
 minWordLength_absolute = 2
-maxCandidates = 3
+maxCandidates = 5
 profiled = decorators.profiled
 
 @profile
@@ -53,27 +53,30 @@ def generateCrossword(size, lexicon, wordLookup):
         
         # decide whether a dummy character is needed in the beginning of the word
         startdummy = shouldStartDummy(startRow, startCol, across, grid)
+        # adjust conditions to take startdummy into account
+        conditions = map(lambda x: (x[0]-startdummy, x[1]), conditions) # one square offset if startdummy
+        conditions = filter(lambda x: x[0]>=0, conditions) # throw away conditions in negative positions
           
-        # calculate the maximum and minimum length of the word 
-        allowedWordLengths = getAllowedWordLengths(maxHeight, maxWidth, startRow, startCol, across, conditions)
-
-        if max(allowedWordLengths) < minWordLength_absolute: continue #again, can't have one-letter words
-        
-        conditions = filter(lambda x: x[1]!=dummy, conditions) # dummy conditions are not needed (I think)
-        
+        # calculate the allowed word lengths 
+        allowedWordLengths = getAllowedWordLengths(maxHeight, maxWidth, startRow, startCol, across, conditions, startdummy)
+      
+        # dummy conditions were needed to get valid lengths
+        # but are not needed to match words
+        conditions = filter(lambda x: x[1]!=dummy, conditions) 
+      
         #find a word that satisfies the above conditions
         wordFound = False
         # iterate through all valid word lengths
         for wordLength in reversed(allowedWordLengths):
-            # words of maxWordLength should only be considered if we don't need a dummy char at the beginning of the word
-            if startdummy and wordLength == max(allowedWordLengths): continue 
+            if wordLength < minWordLength_absolute: continue #again, can't have one-letter words 
             subLexicon = lexicon[wordLength]
             # only conditions up to the wordLength-th word need apply
             subConditions = filter(lambda x: x[0]<wordLength, conditions) 
             # find all the words that satisfy all conditions
             fittingWords = getFittingWords(subConditions, startdummy, subLexicon)
-            if len(fittingWords) > 0:
-                term = random.choice(fittingWords) # choose one of the fitting words randomly
+            random.shuffle(fittingWords)
+            while len(fittingWords) > 0 and not wordFound:
+                term = fittingWords.pop() # choose one of the fitting words randomly
                 terms[(startRow, startCol, across)] = term # add new word to list of terms
                 # add dummy chars at the beginning and end of the new word as necessary
                 if startdummy: term = dummy + term 
@@ -82,7 +85,9 @@ def generateCrossword(size, lexicon, wordLookup):
                 try:
                     grid = placeTermToGrid(term, startRow, startCol, across, grid)
                 except:
-                    pass
+                    print term, startRow, startCol, across
+                    printCrossWord(grid)
+                    raise
                 newState = (copy.deepcopy(terms), copy.deepcopy(grid))
                 if newState not in deadEndStates: # reject if we have already tried this word
                     frontStates.append(newState)
@@ -94,9 +99,8 @@ def generateCrossword(size, lexicon, wordLookup):
                     
         if not wordFound: # we need to backtrack!
             deadEndStates.append( frontStates.pop() )
-    for t, g in frontStates + deadEndStates: #find best state!
-        if len(t) > len(terms):
-            terms, grid = t, g
+    terms, grid = getBestState(terms, grid, frontStates + deadEndStates) #find best state
+    
     print "\t[DONE]"
     return grid, terms
 
@@ -111,26 +115,40 @@ def getStartPos(startOrder, grid, terms):
                 startCandidates.append((startRow, startCol, across))    
     #find all crossing points with pre-existing words for each candidate
     condList = map(lambda x: getConditions(grid, x[0], x[1], x[2]), startCandidates)
-    # best candidate is the one with the most crossings
+    
+    # the more crossings a candidate has the better 
     condLengths = map(len, condList)
-    bestPosIndex = condLengths.index(max(condLengths))
-    startRow, startCol, across = startCandidates[bestPosIndex]
-    conditions = condList[bestPosIndex]
+    # choose start-position with probability proportional to the number of crossings each candidate has
+    power = 3 # the higher this number the higher the probability at the best candidates
+    roulette = random.randint(0, sum(map(lambda x:x**power, condLengths)))
+    sumSoFar = 0
+    for i, cond in enumerate(condList):
+        sumSoFar += len(cond)**power
+        if sumSoFar >= roulette:
+            startRow, startCol, across = startCandidates[i]
+            conditions = cond
+            break
+            
     return startRow, startCol, across, conditions
 
 @profile
 def isValidStart(grid, terms, startRow, startCol, across):
+    # unless we are at the edge, starting square must be free or dummy
+    isEdge = startCol == 0 if across else startRow == 0 
+    if not isEdge and len(grid[startRow][startCol]) > 0 and grid[startRow][startCol] != dummy:
+        return False
     # iterate backwards beginning from the starting position
     it = reversed(range( (startCol if across else startRow) + 1 )) 
     for i in it:
         r, c = (startRow, i) if across else (i, startCol)
         # if you find a starting position of a previous word 
         # before you find a dummy char or the edge of the crossword
-        #then this starting position is invalid 
+        # then this starting position is invalid 
         if (r, c, across) in terms:
             return False
         elif grid[r][c] == dummy:
             break
+                
     return True
 
 @profile
@@ -152,9 +170,11 @@ def shouldStartDummy(startRow, startCol, across, grid):
     return startdummy
 
 @profile        
-def getAllowedWordLengths(maxHeight, maxWidth, startRow, startCol, across, conditions):
+def getAllowedWordLengths(maxHeight, maxWidth, startRow, startCol, across, conditions, startdummy):
     allowed = []
-    minWordLength = 1
+    # if we have a startdummy, pretend the starting position is moved by one square 
+    startRow, startCol = (startRow, startCol+startdummy) if across else (startRow+startdummy, startCol)
+    minWordLength = 0
     while (minWordLength in [x[0] for x in conditions]) and (minWordLength, dummy) not in conditions:
         minWordLength += 1
     for wordLength in range(minWordLength, maxWidth - startCol + 1 if across else maxHeight - startRow + 1):
@@ -169,7 +189,7 @@ def getFittingWords(subConditions, startdummy, subLexicon):
     # calculate the lists of words that satisfy each condition
     # their intersection are the words that satisfy all conditions 
     if len(subConditions) > 0: 
-        fittingWordSets = map(lambda x: subLexicon[ (x[0]-startdummy, x[1]) ], subConditions)
+        fittingWordSets = map(lambda x: subLexicon[x], subConditions)
         fittingWordIds = reduce(lambda x,y: x.intersection(y), fittingWordSets)
     else: # if there are no conditions, take all words!
         fittingWordSets = subLexicon.values()
@@ -183,21 +203,29 @@ def placeTermToGrid(term, startRow, startCol, across, grid):
     # and put in the letters of the term one by one
     for offset, letter in enumerate(term):
         if across:
-            if grid[startRow][startCol + offset] != letter and grid[startRow][startCol + offset] != "":
-                raise Exception("Invalid letter placement: " + letter + " -> " + grid[startRow][startCol + offset])
+            #if grid[startRow][startCol + offset] != letter and grid[startRow][startCol + offset] != "":
+                #raise Exception("Invalid letter placement: " + letter + " -> " + grid[startRow][startCol + offset])
             grid[startRow][startCol + offset] = letter
         else:
-            if grid[startRow + offset][startCol] != letter and grid[startRow + offset][startCol] != "":
-                raise Exception("Invalid letter placement: " + letter + " -> " + grid[startRow + offset][startCol])
+            #if grid[startRow + offset][startCol] != letter and grid[startRow + offset][startCol] != "":
+                #raise Exception("Invalid letter placement: " + letter + " -> " + grid[startRow + offset][startCol])
             grid[startRow + offset][startCol] = letter
     return grid
-    
+
+@profile
+def getBestState(terms, grid, states):
+    for t, g in states: 
+        # add up the letters of all the terms
+        # this rewards crosswords that have many words with many letters each
+        if sum(map(len,t.values())) > sum(map(len,terms.values())):
+            terms, grid = t, g
+    return terms, grid    
 
 def printCrossWord(grid):
     print "\nCrossword:\n\n" + "\n".join(' '.join([x if len(x)>0 else '_' for x in row]) for row in grid)
 
 lexicon, wordLookup = importDictionary()
-grid, terms = generateCrossword(8, lexicon, wordLookup)
+grid, terms = generateCrossword(7, lexicon, wordLookup)
 printCrossWord(grid)
-print "\nTerms: " + ', '.join(terms.values())
+print "\nTerms: " + ', '.join(terms.values()) + "\n"
 decorators.printProfiled()
